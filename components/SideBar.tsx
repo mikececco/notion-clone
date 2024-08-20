e4edaa05032de450578f98e12e2c9c04f0cb883c
Mike Cecconello <mikececco2000@gmail.com>
2024-08-20 21:41:16 +0200

    Sidebar init

---
+++ components/SideBar.tsx
@@ -0,0 +1,46 @@
+import { MenuIcon } from "lucide-react"
+import NewDocumentButton from "./NewDocumentButton"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+
+
+function SideBar() {
+
+  const menuOptions = (
+    <>
+      <NewDocumentButton/>
+    </>
+  )
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className=""/>
+          </SheetTrigger>
+          <SheetContent side='left'>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>
+                {menuOptions}
+              </div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="hidden md:inline">
+        {menuOptions}
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
