716e8a0829cf5e0da03e41fe428d89efce11d50e
Mike Cecconello <mikececco2000@gmail.com>
2024-08-25 16:08:35 +0200

    GroupedData function

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -12,13 +12,27 @@ import {
 } from "@/components/ui/sheet"
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { useUser } from "@clerk/nextjs"
-import { collectionGroup, query, where } from "firebase/firestore"
+import { collectionGroup, DocumentData, query, where } from "firebase/firestore"
 import { db } from "@/firebase"
+import { useEffect, useState } from "react"
 
+interface RoomDocument extends DocumentData {
+  createdAt: string;
+  role: 'owner' | 'editor';
+  roomId: string;
+  userId: string;
 
+}
 
 function SideBar() {
   const {user} = useUser() //from clerk
+  const [grouped, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  })
 
   const [data, loading, error] = useCollection(
     user &&
@@ -27,7 +41,41 @@ function SideBar() {
         collectionGroup(db, 'rooms'),
         where('userId', '==', user.emailAddresses[0].toString())
       )
+  )
+
+  useEffect(() => {
+    if (!data) return
+
+    const grouped = data.docs.reduce<{ //accumulator function
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, curr) => {
+        const roomData = curr.data() as RoomDocument;
+
+        if (roomData.role === 'owner') {
+          acc.owner.push({
+            id: curr.id,
+            ...roomData,
+          });
+        } else {
+          acc.editor.push({
+            id: curr.id,
+            ...roomData,
+          });
+        }
+
+        return acc
+      }, {
+        owner: [],
+        editor: [],
+      }
     )
+
+    setGroupedData(grouped)
+  
+  }, [data])
+
 
 
   const menuOptions = (
