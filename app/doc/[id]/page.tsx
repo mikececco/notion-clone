19bff4b9b41f1f80a3dc895584902a729f1e70e0
Mike Cecconello <mikececco2000@gmail.com>
2024-08-26 21:28:44 +0200

    Document page component init: 1:53:23 / 5:05:12

--- app/doc/[id]/page.tsx
+++ app/doc/[id]/page.tsx
@@ -1,6 +1,16 @@
-function DocumentPage() {
+'use client'
+
+import Document from "@/components/Document"
+
+function DocumentPage({params: {id}}: {
+  params: {
+    id: string
+  }
+}) {
   return (
-    <div>DocumentPage</div>
+    <div className="flex flex-col flex-1 min-h-screen">
+      <Document id={id}/>
+    </div>
   )
 }
 
---
+++ components/Document.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+function Document({id}:  {id: string}) {
+
+  return (
+    <div>Document</div>
+  )
+}
+
+export default Document
