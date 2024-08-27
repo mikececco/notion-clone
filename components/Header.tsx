037a59cf5f220a342bf8438954ce08a6a20c7153
Mike Cecconello <mikececco2000@gmail.com>
2024-08-27 21:25:11 +0200

    Breadcrumbs

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 import { useUser } from "@clerk/nextjs"
+import BreadCrumbs from "./BreadCrumbs"
 
 function Header() {
 
@@ -14,6 +15,7 @@ function Header() {
       )}
 
       {/* BREADCRUMB */}
+      <BreadCrumbs />
 
       <div>
         <SignedOut>
