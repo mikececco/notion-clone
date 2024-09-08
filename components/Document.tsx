1e1691df3e01307b325e6e508fb1d6843184809b
Mike Cecconello <mikececco2000@gmail.com>
2024-09-08 20:16:32 +0200

    Working Editor with Blocknote + Liveblocks

--- components/Document.tsx
+++ components/Document.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button"
 import { doc, updateDoc } from "firebase/firestore"
 import { db } from "@/firebase"
 import { useDocumentData } from "react-firebase-hooks/firestore"
+import Editor from "./Editor"
 
 function Document({id}:  {id: string}) {
   const [data, loading, error] = useDocumentData(doc(db, 'documents', id))
@@ -53,7 +54,10 @@ function Document({id}:  {id: string}) {
         {/* Avatars */}
       </div>
 
+      <hr className="pb-10"/>
+
       {/* Collaborative Editor */}
+      <Editor />
 
     </div>
   )
